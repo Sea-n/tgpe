e9a026d7cf79b3e5829e2d5f8e6c4d5f5e10ac8b
Sean Wei <me@sean.taipei>
2019-09-14 14:51:59 +0800

    Add HTTP API endpoint

---
+++ api.php
@@ -0,0 +1,35 @@
+<?php
+require('config.php');
+require('database.php');
+
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+	header("HTTP/2 405 Method Not Allowed");
+	exit;
+}
+
+if (!array_key_exists($_POST['token'] ?? 'guest', HTTP_API_TOKENS)) {
+	header("HTTP/2 401 Unauthorized");
+	exit;
+}
+
+if (!isset($_POST['url']))
+	exit('No url');
+
+if (!isset($_POST['code']))
+	exit('No code');
+
+$db = new MyDB();
+$error = $db->insert($url, $code, 777000);
+
+if ($error[0] === '00000')
+	echo json_encode([
+		'ok' => true,
+		'shortLink' => $code
+	], JSON_PRETTY_PRINT);
+else
+	echo json_encode([
+		'ok' => false,
+		'errorCode1' => $error[0],
+		'errorCode2' => $error[1],
+		'errorInfo' => $error[2],
+	], JSON_PRETTY_PRINT);
--- config-example.php
+++ config-example.php
@@ -2,3 +2,8 @@
 define('TG_ADMINS', [
 	109780439  # Sean
 ]);
+
+define('HTTP_API_TOKENS', [
+	'guest' => 'guest',
+	'Base64TokenOrAnySecretStringOnlyMeKnows' => 'admin'
+]);
